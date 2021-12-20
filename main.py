4ae30f4b660e90790903b9c7fe90d5c4d6a3a895
Alexandria <alxpettit@gmail.com>
2021-12-19 19:35:39 -0800

    point class

--- main.py
+++ main.py
@@ -10,35 +10,40 @@ from pydantic import BaseModel
 
 class Point:
     x: int
-    y: int 
+    y: int
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 class GridSettings(BaseModel):
     bg_color: str
     primary_color: str
     secondary_color: str
-    approx_dims_x: int
-    approx_dims_y: int
-    spacing_x: int
-    spacing_y: int
+    approx_dims: Point
+    spacing: Point
     line_width_primary: int
     line_width_secondary: int
     grid_ratio: int
 
     @classmethod
     def from_gui(cls):
+        approximate_dimensions = Point(st.number_input('Approx X', value=1024), st.number_input('Approx Y', value=768))
+        spacing = Point(st.number_input('X spacing', value=100), st.number_input('Y spacing', value=100))
         return cls(
             bg_color=st.color_picker('Background color', '#FFFFFF'),
             primary_color=st.color_picker('Primary grid color', '#000000'),
             secondary_color=st.color_picker('Secondary grid color', '#555555'),
-            spacing_x=st.number_input('X spacing', value=100),
-            spacing_y=st.number_input('Y spacing', value=100),
-            approx_dims_x=st.number_input('Approx X', value=1024),
-            approx_dims_y=st.number_input('Approx Y', value=768),
+            spacing=spacing,
+            approx_dims=approximate_dimensions,
             line_width_primary=st.number_input('Line width, primary', value=3),
             line_width_secondary=st.number_input('Line width, secondary', value=1),
             grid_ratio=st.number_input('Length of large square, in small squares', value=2)
         )
+
+    class Config:
+        arbitrary_types_allowed = True
 
 
 class DrawGrids:
@@ -49,14 +54,14 @@ class DrawGrids:
         # FYI I know this is horrible but I was rushing and didn't want to implement a proper Point class.
 
         # Calculate amount of spacing to put between the lines on the primary grid (grid1)
-        self.spacing_grid1_x = self.conf.spacing_x * self.conf.grid_ratio
-        self.spacing_grid1_y = self.conf.spacing_y * self.conf.grid_ratio
+        self.spacing_grid1_x = self.conf.spacing.x * self.conf.grid_ratio
+        self.spacing_grid1_y = self.conf.spacing.y * self.conf.grid_ratio
 
-        self.x = self.conf.approx_dims_x - (self.conf.approx_dims_x % self.spacing_grid1_x)
-        self.y = self.conf.approx_dims_y - (self.conf.approx_dims_y % self.spacing_grid1_y)
+        self.x = self.conf.approx_dims.x - (self.conf.approx_dims.x % self.spacing_grid1_x)
+        self.y = self.conf.approx_dims.y - (self.conf.approx_dims.y % self.spacing_grid1_y)
 
-        self.num_squares_x = int(self.x / self.conf.spacing_x)
-        self.num_squares_y = int(self.y / self.conf.spacing_y)
+        self.num_squares_x = int(self.x / self.conf.spacing.x)
+        self.num_squares_y = int(self.y / self.conf.spacing.y)
 
         self.square_num_grid1_x = int(self.num_squares_x / self.conf.grid_ratio)
         self.square_num_grid1_y = int(self.num_squares_y / self.conf.grid_ratio)
@@ -67,10 +72,10 @@ class DrawGrids:
     def draw_grid2(self):
         """ The secondary grid is the small, usually lighter colored grid intended to measure smaller units. """
         for i in range(1, self.num_squares_y):
-            i *= self.conf.spacing_y
+            i *= self.conf.spacing.y
             self.canvas.line((0, i, self.x, i), fill=self.conf.secondary_color, width=self.conf.line_width_secondary)
         for i in range(1, self.num_squares_x):
-            i *= self.conf.spacing_x
+            i *= self.conf.spacing.x
             self.canvas.line((i, 0, i, self.y), fill=self.conf.secondary_color, width=self.conf.line_width_secondary)
 
     def draw_grid1(self):
